c116e74b903541a61658f8226c557cd75f48eb79
Varshaa A M <124232595+vxrshxx@users.noreply.github.com>
2023-11-26 12:34:21 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-background">
+        <h2 className="top-right-text">Welcome to the home page of "PEARL AND LUSH" !</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
